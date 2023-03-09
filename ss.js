24f04a715188633f938d7753699b3cb9b2440cab
italaliano <127443276+italaliano@users.noreply.github.com>
2023-03-09 21:53:54 +0200

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,22 @@
+for (let index = 3; index < 9; index++) {
+    document.getElementById('h'+index.toString()).style.height = '100px';
+    document.getElementById('h'+index.toString()).style.width = '100px';
+}
+var eg = () => {
+ document.body.style.backgroundImage = 'url("eg.jpg")'
+}
+var ch = () => {
+    document.body.style.backgroundImage = 'url("ch.gif")'
+}
+var usa = () => {
+    document.body.style.backgroundImage = 'url("am.jpg")'
+}
+var s_a = () => {
+    document.body.style.backgroundImage = 'url("s_a.jpg")'
+}
+var is = () => {
+    document.body.style.backgroundImage = 'url("hetl.jpg")'
+}
+var ger = () => {
+    document.body.style.backgroundImage = 'url("ger.jpg")'
+}
